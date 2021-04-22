aa2ee42f8253740694d5ae722b205be94c2fe642
Bhavikaaa <82111793+Bhavikaaa@users.noreply.github.com>
2021-04-22 17:37:14 +0530

    Task 1

---
+++ Task 1.cpp
@@ -0,0 +1,14 @@
+//C++ Program to find size of int, float, double and char. 
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"Size od char: 1 byte";
+cout<<"Size of int: 4 bytes";
+cout<<"Size of float: 4 bytes";
+cout<<"Size of double:8 bytes";
+return 0;	
+{git remote add origin https://github.com/Bhavikaaa/C-Basics.git
+git branch -M main
+git push -u origin main}
+}
